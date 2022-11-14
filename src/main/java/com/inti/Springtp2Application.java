0dc31cc68f4379e3383000539c702cd0624765ad
theolorenzi <theo.lorenzi@laposte.net>
2022-11-14 09:36:02 +0100

    First commit

---
+++ src/main/java/com/inti/Springtp2Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtp2Application.class, args);
+	}
+
+}
